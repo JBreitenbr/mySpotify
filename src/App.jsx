4ab21a343271b83d1942a618a1ed1b7a81e74235
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2024-09-19 07:20:05 +0000

    added responsiveness to tooltips

--- src/App.jsx
+++ src/App.jsx
@@ -21,11 +21,11 @@ tDict[bandName].map(
 return (
  <><li key={index} >
    <div className="ml-4 mb-4"><a data-tooltip-id={`my-anchor-element-${index}`}>
-    <img src={item.imgUrl} className="h-20 w-20 sm:h-32 sm:w-32" style={{border:"1px solid #0f172a"}}/><Tooltip id={`my-anchor-element-${index}`}> <div style={{width:"80vw"}}><div><img src={item.imgUrl2} className="h-32 w-32"/><p className="text-md">{item.album}</p><p className="text-md">{item.release_date}</p></div><div><p className="text-xs">{item.tracks}</p></div></div></Tooltip></a>
+    <img src={item.imgUrl} className="h-20 w-20 sm:h-32 sm:w-32" style={{border:"1px solid #0f172a"}}/><Tooltip id={`my-anchor-element-${index}`}> <div className="w-64 sm:w-72 md:w-80 lg:w-96"><div><img src={item.imgUrl2} className="h-32 w-32 sm:h-40 sm:w-40 lg:h-56 lg:w-56"/><p className="text-base sm:text-xl">{item.album}</p><p className="text-base sm:text-xl">{item.release_date}</p></div><div><p className="text-xs sm:text-sm">{item.tracks}</p></div></div></Tooltip></a>
    <a data-tooltip-id={`tooltip_${index}`} key={index}>
-    <div className="small-txt sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl mt-4 text-white">{item.track}</div>
+    <div className="small-txt sm:text-xl md:text-2xl xl:text-3xl mt-4 text-white">{item.track}</div>
     <Tooltip id={`tooltip_${index}`} key={index}>
-  <div className="tooltip">
+  <div className="tooltip p-4 sm:p-6">
     <ul>
       <li>Track: {item.track}</li>
       <li>Released: {item.release_date}</li>
@@ -48,18 +48,18 @@ return (
     <div className="generale bg-slate-500" style={{position:"relative"}}>
      <div className="bg-slate-300 flex flex-col" style={{position: "sticky",top:"0px",width:"100vw"}}>
 <h3 className="text-white text-center text-6xl h-32 pt-8 mb-6 test" >Julias Spotify</h3>
-      <select className="mx-8" onChange={handleLetter}>
+      <select className="mx-8 sm:mx-32 sm:h-6 sm:text-xl" onChange={handleLetter}>
       <option>--Select Letter--</option>
         {bandsDict.map(item=>
           <option key={item.name}>{item.name}</option>
         )}
       </select>
-      <select className="mb-6 mx-8" onChange={handleBand}>
-        <option>--Select Band--</option>
+      <select className="mb-6 mx-8 sm:mx-32 sm:h-6 sm:text-xl" onChange={handleBand}>
+        <option>--Select Artist--</option>
         {bands.map(item=>
           <option key={item.name}>{item.name}</option>
         )}</select></div><br/>
-      {bandName=="none"?<div className="spoti mt-9 sm:mt-36"></div>:<ul className="grid grid-cols-2">{topItems}</ul>}
+      {bandName=="none"?<div className="spoti mt-7 sm:mt-36"></div>:<ul className="grid grid-cols-2">{topItems}</ul>}
     </div>
   )
 }
